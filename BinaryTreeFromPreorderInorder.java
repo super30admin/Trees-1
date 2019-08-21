4b20427c0170c2fe26719693f5969070ee0a4f42
Manogna Mujje <manogna.mujje@gmail.com>
2019-08-20 19:30:44 -0700

    Optimized problem-2

--- BinaryTreeFromPreorderInorder.java
+++ BinaryTreeFromPreorderInorder.java
@@ -1,8 +1,11 @@
 package binaryTrees;
 
 import java.util.Arrays;
+import java.util.HashMap;
 /**
- * Approach: Split left subtree elements and right subtree elements and repeatedly perform the buidtree function
+ * PreOrder: root->left->right
+ * InOrder: left->root->right
+ * Approach: Split left subtree elements and right subtree elements and repeatedly perform the buildtree function
  * 
  */
 public class BinaryTreeFromPreorderInorder {
@@ -40,6 +43,33 @@ public class BinaryTreeFromPreorderInorder {
 		int[] inorder = {9,3,15,20,7};
 		
 		buildTree(preorder, inorder);
+		buildTreeOptimized(preorder, inorder);
+	}
+	
+	public static TreeNode buildTreeOptimized(int[] preorder, int[] inorder) {
+		 if(preorder.length == 0) return null;
+		 
+		 //Store inorder elements in Hashmap with index as the value in order to retrieve position of an element in O(1) time
+		 HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();	 
+		 for(int i=0; i<inorder.length; i++) {
+			 map.put(inorder[i], i);
+		 }
+		 return helper(0, inorder.length-1, preorder, 0, preorder.length-1, map);
+	}
+	
+	private static TreeNode helper(int instart, int inend, int[] preorder, int prestart, int preend, HashMap<Integer, Integer>map) {
+		//Base case
+		if(prestart > preend) return null;
+		//First Element in preorder traversal is always root
+		 TreeNode root = new TreeNode(preorder[prestart]);
+		 int rootIndex = map.get(root.val);
+		 
+		 //Size of left subtree
+		 int leftSize = rootIndex - instart;
+		 
+		 root.left = helper(instart, rootIndex-1, preorder, prestart+1, prestart+leftSize, map);
+		 root.right = helper(rootIndex+1,inend , preorder, prestart+leftSize+1, preend, map);
+		 return root;
 	}
 
 }
