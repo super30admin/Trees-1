31d45f0b9f0a0c5f464718008780854c3258dda4
Rakshitha94 <rakshithalr3007@gmail.com>
2019-09-12 12:36:50 -0700

    Completed

--- buildtree.py
+++ buildtree.py
@@ -1,7 +1,11 @@
-# time complexity:
-# leetcode:
-# explaination:
-# doubts
+# time complexity: O(N)
+# space complexity:O(n)
+# leetcode: accepted
+# explaination: in preorder traversal the root always at the front. Find the root element and its index in the inorder traversal
+# elements left to the root element forms the left subtree and elements towards the riht form theright subtree.
+# left subtree = 0 to index1 and right subtree = index+1 till end of array. Keep on continu you reach ibase case that is len(preorder) is 0
+# doubts: None
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -13,18 +17,17 @@ class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         if len(preorder) == 0:
             return None
-        self.root = TreeNode(preorder[0])
+        root = TreeNode(preorder[0])
         # print(self.root.val)
-        index = -1
+        index = None
         for i in range(len(inorder)):
-            if inorder[i] == (self.root.val):
+            if inorder[i] == (root.val):
                 index = i
-            print(index)
         preleft = preorder[1:index + 1]
         inleft = inorder[0:index]
         preright = preorder[index + 1:len(preorder)]
         inright = inorder[index + 1:len(preorder)]
-        self.root.left = self.buildTree(preleft, inleft)
-        self.root.right = self.buildTree(preright, inright)
-        return self.root
+        root.left = self.buildTree(preleft, inleft)
+        root.right = self.buildTree(preright, inright)
+        return root
 
--- isvalidbst.py
+++ isvalidbst.py
@@ -1,3 +1,11 @@
+# time complextiy: o(n)
+# leetcode: accepted
+# explainatin: since it is a binarry tree, the left subtree can have values ranging
+# from (-infintiy, root.val), beacse the left subtree should always be smaller than the root valuw and
+# the right subtree can have values ranging from (roor.val,+ infinity) . right subtree should be greater than the root value.
+# We check these conditions for every node recursively
+# no doubts
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -7,12 +15,13 @@
 
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
-        return self.isvalidBool(root, None, None)
 
-    def isvalidBool(self, root, minimum, maximum):
-        if root == None:
-            return True
-        elif (minimum != None and minimum >= root.val) or (maximum != None and maximum <= root.val):
-            return False
-        else:
-            (self.isvalidBool(root.left, minimum, root.val)) and (self.isvalidBool(root.right, root.val, maximum))
+        def isvalidBool(root, minimum, maximum):
+            if root == None:
+                return True
+            elif (minimum != None and minimum >= root.val) or (maximum != None and maximum <= root.val):
+                return False
+            else:
+                return (isvalidBool(root.left, minimum, root.val) and isvalidBool(root.right, root.val, maximum))
+
+        return isvalidBool(root, None, None)
