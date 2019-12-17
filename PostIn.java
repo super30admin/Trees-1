388dddf810675576d10f8bb25516fe7138b7ccd4
Akhil Raveendran <itsrakhil163@gmail.com>
2019-12-17 00:58:34 -0800

    PreIn.java

--- PostIn.java
+++ PostIn.java
@@ -2,24 +2,23 @@ Time Complexity-O(n)
 Space Complexity-O(1)
 
 class Solution {
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-    return helper(inorder, inorder.length-1, 0, postorder, postorder.length-1);
-}
-
-private TreeNode helper(int[] inorder, int start, int end, int[] postorder,
-		int pStart) {
-	if (pStart < 0 || start < end)
-		return null;
-	TreeNode root = new TreeNode(postorder[pStart]);
-	int index = start;
-	for (int i = start; i >= end; i--) {
-		if (inorder[i] == postorder[pStart]) {
-			index = i;
-			break;
-		}
-	}
-	root.right = helper(inorder, start, index+1, postorder, pStart-1);
-	root.left = helper(inorder, index - 1, end, postorder, pStart - (start - index) -1);
-	return root;
-}
+public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTree(preorder, 0, inorder, inorder.length - 1, 0);
+    }
+    
+    private TreeNode buildTree(int[] preorder, int index, int[] inorder, int end, int start) {
+        if (index >= preorder.length || start > end) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[index]);
+        int i;
+        for (i = end; i >= start; i--) {
+            if (preorder[index] == inorder[i]) {
+                break;
+            }
+        }
+        root.left = buildTree(preorder, index + 1, inorder, i - 1, start);
+        root.right = buildTree(preorder, index + i - start + 1, inorder, end, i+1);
+        return root;
+    }
 }
