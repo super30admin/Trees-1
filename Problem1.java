6bf5dd5c1c0e5eae6b82578b7d2f80fa88ef82e5
Sweety Sojrani <sweety.sojrani@sjsu.edu>
2019-12-28 13:22:56 +0800

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,3 +1,7 @@
+// Time Complexity - O(n) where n is the number of elements in the tree
+// Space Complexity - O(1) since we are not using any extra space
+// This solution worked on LeetCode
+// For this problem, we would be using the property of BST that all the left subtrees values will be smaller than the root value and all the right subtree values will be greater than the root value. This will be taken care using min and max value where min for the left subtree will be same as the min of root and the max value will be the root value. Similarly for right subtree the min value will be root value and the max will be same max as that of the root. Below solution is the recursive solution where null is used as min and max for the root. 
 
 
 class Solution {
