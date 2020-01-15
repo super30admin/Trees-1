181468c9f7ecc8ee012156cb0d1d295c22bf8ba8
Nikhil Agrawal <nikrasiya@gmail.com>
2020-01-15 10:29:56 -0500

    all problems complete

--- 98_validate_binary_search_tree.py
+++ 98_validate_binary_search_tree.py
@@ -1,17 +1,20 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+from math import inf
 
 
 class Solution:
+    # Definition for a binary tree node.
+    class TreeNode:
+        def __init__(self, x):
+            self.val = x
+            self.left = None
+            self.right = None
+
     def isValidBST(self, root: TreeNode) -> bool:
         """
             // Time Complexity : O(n)
                     'n' is the number of nodes
-            // Space Complexity : O(n)
+            // Space Complexity : O(h)
+                    'h' is the height of the tree
             // Did this code successfully run on Leetcode : Yes
             // Any problem you faced while coding this : No
             // Three line explanation of solution in plain english:
@@ -20,19 +23,59 @@ class Solution:
                   not a valid BST
         """
         # in order traversal LDR
-        cur = root
         stack = []
-        result = []
-        while cur or stack:
+        prev = -inf
+        while root or stack:
             # if we have a left child
-            if cur:
-                stack.append(cur)
-                cur = cur.left
+            if root:
+                stack.append(root)
+                root = root.left
             else:
                 # pop data
                 cur = stack.pop()
-                if result and result[-1] >= cur.val:
+                if prev >= cur.val:
                     return False
-                result.append(cur.val)
-                cur = cur.right
+                prev = cur.val
+                root = cur.right
         return True
+
+    prev = -inf
+
+    def isValidBST_inorder_recursive(self, root: TreeNode) -> bool:
+        """
+            // Time Complexity : O(n)
+                    'n' is the number of nodes
+            // Space Complexity : O(h) Recursive stack space
+                    'h' is the height of the tree
+            // Did this code successfully run on Leetcode : Yes
+        """
+        return self._helper_recursive(root)
+
+    # LDR
+    def _helper_recursive(self, root: TreeNode):
+        if root is None: return True
+        if not self._helper_recursive(root.left): return False
+        if self.prev >= root.val: return False
+        self.prev = root.val
+        return self._helper_recursive(root.right)
+
+    def isValidBST_recursive(self, root: TreeNode) -> bool:
+        """
+            // Time Complexity : O(n)
+                    'n' is the number of nodes
+            // Space Complexity : O(h) Recursive stack space
+                    'h' is the height of the tree
+            // Did this code successfully run on Leetcode : Yes
+        """
+        # root can have range of values from -inf to inf
+        return self._helper(root, -inf, inf)
+
+    def _helper(self, root: TreeNode, min: int, max: int):
+        # base
+        # we have reached the children so it is a valid sub tree
+        if root is None: return True
+        # if the root value is greater than maximum allowed
+        # if the root value is lesser than minimum allowed
+        if root.val >= max or root.val <= min: return False
+        # logic
+        return self._helper(root.left, min, root.val) and self._helper(root.right, root.val, max)
