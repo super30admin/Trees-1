c14d77e3fe306994d98cdbdb406d270e820e22a3
Sumukh Ballal <sumukh.ballal@appdynamics.com>
2020-06-13 19:00:54 -0700

    AUTO

--- validBST.java
+++ validBST.java
@@ -1,5 +1,11 @@
 import java.util.LinkedList;
 import java.util.Queue;
+// THe funciton is named checkBST , please give me feedback for my approach
+// I chose a recursive min max approach which uses a helper function.
+// It checks left and right subtree recursively while maintaining min and max values
+// Time Complexity : O(n) where n= all values in the array 
+
+// Space Complexity : O(n) where at any given time there might be N total functions in the Stack
 
 class Node
 {
@@ -27,38 +33,32 @@ public class validBST
     public static void main(String args[]) {
         // Assume -1 -1 to be null
         //int[] arr={5,1,2,-1,-1,4,5};
-        int[] arr={5,1,4,-1,-1,3,6};
+        int[] arr={5,1,7,-1,3,6,8};
         validBST obj=new validBST();
         Node node=obj.makeTree(arr);
-        //obj.traceTree(node);
+       // obj.traceTree(node);
         System.out.println(obj.checkBST(node));
     }
 
     public boolean checkBST(Node head)
     {
         Node traverse=head;
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(traverse);
-
-        while(!queue.isEmpty())
-        {
-            traverse=queue.poll();
-            int rootV=traverse.value;
-            if(traverse.left != null && traverse.right!=null)
-            {
-                int leftV=traverse.left.value;
-                int rightV=traverse.right.value;
-
-                if(leftV>rootV || rightV<rootV)
-                    return false;
-
-                queue.add(traverse.left);
-                queue.add(traverse.right);
-            }
-        }
-
-        return true;
+        return checkBST_helper(traverse,Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
+
+    public boolean checkBST_helper(Node traverse, int min, int max)
+    {
+        if(traverse==null)
+            return true;
+        
+        int value=traverse.value;
+        if(value<min || value>max)
+        {
+            System.out.println("Returning false for value "+value+ " min "+min+" max "+max);
+            return false;
+        }
+        return checkBST_helper(traverse.left, min, value-1) && checkBST_helper(traverse.right, value+1, max);
+    }   
 
     public void traceTree(Node head)
     {
