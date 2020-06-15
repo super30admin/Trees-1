adbe10f789a8b7ff0b088fc0282da93774b02f7f
Prashanth Chintala <pchintala16@gmail.com>
2020-06-15 14:56:57 -0700

    Completed Valid BST

---
+++ ValidBST.java
@@ -0,0 +1,30 @@
+// Time Complexity : O(n). n is the number of nodes in a tree
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+       return binaryTree(root);
+    }
+    
+    private boolean binaryTree(TreeNode root){
+        if(root==null) return true;
+        Stack<TreeNode>stack = new Stack();
+        TreeNode prevNode = null;
+        
+        while(root!=null || !stack.isEmpty()){
+            while(root!=null){
+                stack.push(root);
+                root=root.left;
+            }
+            root=stack.pop();
+            if(prevNode!=null && root.val<=prevNode.val) return false;
+            prevNode=root;
+            root=root.right;
+        }
+        return true;
+        
+    }
+}
