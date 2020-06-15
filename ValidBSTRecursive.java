651197bd4e74b3bb88bfd207da78f3d56f40bd8e
Prashanth Chintala <pchintala16@gmail.com>
2020-06-15 15:53:46 -0700

    Completed Valid BST Recursive

---
+++ ValidBSTRecursive.java
@@ -0,0 +1,22 @@
+// Time Complexity : O(n). n is the number of nodes in a tree
+// Space Complexity : O(1).
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+class Solution {
+    TreeNode prevNode;
+    public boolean isValidBST(TreeNode root) {
+       return binaryTree(root);
+    }
+    
+    private boolean binaryTree(TreeNode root){
+        if(root==null) return true;
+        
+       if(binaryTree(root.left)==false) return false;
+        if(prevNode!=null && root.val<=prevNode.val) return false;
+        prevNode=root;
+        return binaryTree(root.right);
+        
+    }
+}
