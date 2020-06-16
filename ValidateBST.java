a13e21f3cc87358100ae41c2dca738f81598771e
chirag <cr.tagadiya@gmail.com>
2020-06-16 15:32:53 -0400

    Validate BST recursive solution added

--- ValidateBST.java
+++ ValidateBST.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 
 /*
+    
     Time Complexity : O(N)
     Space Complexity :  No Extra Space Recursive space O(H) H is the height of the tree
     Is worked on leetcode : YES
@@ -36,7 +37,27 @@ class TreeNode {
  * }
  */
 public class ValidateBST {
-    public boolean isValidBST(TreeNode root) {
+
+    TreeNode prev_node;
+    public boolean isValidBST_Recursive(TreeNode root) {
+        return inorder(root);
+    }
+    
+    private boolean inorder(TreeNode root){
+        if (root == null )return true;
+        
+//         left subtree
+//         if left subtree is not valid BST retunr false
+        if( inorder(root.left) == false) return false;
+//         check if root is less than or not if it is less than return false
+        if(prev_node != null && root.val <= prev_node.val ) return false;
+        
+        prev_node = root;
+//         right subtree
+        return inorder(root.right);
+        
+    }
+    public boolean isValidBST_Iterative_InorderSolution(TreeNode root) {
         Stack<TreeNode> stack = new Stack();
         TreeNode prev_node=null;
         
