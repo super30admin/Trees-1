8466d4663d53482469887c9a50fe7037cad4e616
Sarthak Sugandhi <47070701+sarthaksugandhi9@users.noreply.github.com>
2020-06-16 23:57:15 -0700

    Create Problem_1_Recursive_2.java

---
+++ Problem_1_Recursive_2.java
@@ -0,0 +1,40 @@
+// Time complexity - O(n)
+// Space complexity - O(h)
+
+// Recusrive solution using inorder traversal and prev pointer.
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode prev;
+    public boolean isValidBST(TreeNode root) {
+       return inorder(root);
+    }
+    private boolean inorder(TreeNode root){
+        if(root == null){
+            return true;
+        }
+        
+        if(inorder(root.left) == false) return false;
+        if(prev != null && prev.val >= root.val){
+            return false;
+        }
+        prev = root;
+        return inorder(root.right);
+    }
+    
+}
