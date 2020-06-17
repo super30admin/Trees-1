40ba7a96f71969320770196e36848deeed98c501
Ashish <gurav.ashish1@gmail.com>
2020-06-16 21:34:31 -0500

    Recursive solution for valid binary

--- ValidBinary.java
+++ ValidBinary.java
@@ -23,3 +23,36 @@ class Solution {
         return true;
     }
 }
+
+//----------Recursive Solution ----------------
+
+
+class Solution {
+    TreeNode prev;
+   public boolean isValidBST(TreeNode root) {
+        return inOrder(root);
+   }
+   
+   private boolean inOrder(TreeNode root){
+       //base 
+       
+       if(root == null) return true;
+       
+       // logic
+      
+       if(inOrder(root.left) == false) return false;
+       
+      // while(root != null || !stack.isEmpty()){
+        //   while(root != null){
+          //     stack.push(root);
+            //   root = root.left;
+               
+           //}
+       
+           
+           //root = stack.pop();
+           if(prev != null && root.val <= prev.val) return false;
+           prev = root;
+          return  inOrder(root.right);
+   }   
+}
