d18b572a6ab05d8535c9ad8b39f52bd8f6ba8b27
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-07-28 23:03:15 -0500

    Update Construct_BT_Preorder_Inorder.java

--- src/Construct_BT_Preorder_Inorder.java
+++ src/Construct_BT_Preorder_Inorder.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /*class TreeNode {
 	int val;
 	TreeNode left;
@@ -11,24 +13,72 @@
 	}
 }*/
 
-class Construct_BT_Preorder_Inorder {
-	public static TreeNode buildTree(int[] preorder, int[] inorder) {
-		return helper(preorder, inorder, 0, 0, inorder.length - 1);
+/******************************Approach 1: optimized*****************************************************/
+//Time Complexity : O(n), where n is the length array(preorder or inorder)
+//Space Complexity : O(n), all nodes with index from the hashmap.
+//Did this code successfully run on Leetcode : yes
+//Any problem you faced while coding this : no
+
+/* Recursively building left and right subtree.
+ * Find the root index from the hashmap and 
+ * Recursively find the left subtree and the right subtree from here by dividing the preorder array based on the index */
+
+class Solution {
+	HashMap<Integer, Integer> map = new HashMap<>();
+	int preOrderIndex = 0;	//root index in preorder
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		for(int i=0; i<inorder.length; i++)
+			map.put(inorder[i], i);
+
+		return helper(preorder, inorder, 0, inorder.length-1);
+
 	}
 
-	public static TreeNode helper(int[]  preorder, int[] inorder,int PreOrderStart, int inOrderStart, int inOrderEnd){
-		if(PreOrderStart > preorder.length - 1 || inOrderStart > inOrderEnd)
+	private TreeNode helper(int[] preorder, int[] inorder, int start, int end){
+		if(start > end || preOrderIndex == preorder.length)		// base condition
 			return null;
 
-		TreeNode root = new TreeNode(preorder[PreOrderStart]);
-		int inOrderIndex = 0;   // index of current root in Inorder
-		for(int i =inOrderStart; i<= inOrderEnd; i++){
-			if(inorder[i] == root.val)
-				inOrderIndex = i;
+		TreeNode root = new TreeNode(preorder[preOrderIndex++]);
+
+		int index = map.get(root.val);	//root index in inorder
+
+		root.left = helper(preorder, inorder, start, index-1);
+		root.right = helper(preorder, inorder, index+1, end);
+
+		return root;
+	}
+}
+
+/***********************************Approach 2: Brute force******************************************************/
+// Time Complexity : O(n^2), where n is the length array(preorder or inorder) * each time searching array for the root
+// Space Complexity : O(n^2), new copy of the array is created at leaf node
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+/* Recursively build left and right subtree
+ * Find the root from preorder (root,left,right) and recursively find the left subtree and the right subtree from here.
+ * At each level we create a new arraycopy because arrays are passed a reference */
+
+class Solution {
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		if(preorder.length == 0 || inorder.length ==0)
+			return null;
+
+		int rootValue = preorder[0];
+		TreeNode root = new TreeNode(rootValue);
+		int index = -1; // root index in inOrder traversal
+		for(int i=0; i<inorder.length; i++){
+			if(inorder[i] == rootValue)
+				index = i;
 		}
 
-		root.left = helper(preorder, inorder, PreOrderStart + 1, inOrderStart, inOrderIndex - 1);
-		root.right = helper(preorder, inorder, PreOrderStart + inOrderIndex - inOrderStart + 1, inOrderIndex + 1, inOrderEnd);
+		int[] preOrderLeft = Arrays.copyOfRange(preorder, 1, index + 1);
+		int[] preOrderRight = Arrays.copyOfRange(preorder, index + 1, preorder.length);
+		int[] InOrderLeft = Arrays.copyOfRange(inorder, 0, index);
+		int[] InOrderRight = Arrays.copyOfRange(inorder, index + 1, inorder.length);
+
+		root.left = buildTree(preOrderLeft, InOrderLeft);
+		root.right = buildTree(preOrderRight, InOrderRight);
 
 		return root;
 	}
