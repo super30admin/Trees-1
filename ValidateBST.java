dc5da60601ced93bd2950b38d3b6d8bc4dae7c28
Arpit Sharma <arpit317@gmail.com>
2020-08-02 12:00:01 -0700

    Update ValidateBST.java

--- ValidateBST.java
+++ ValidateBST.java
@@ -31,3 +31,19 @@ class Solution {
         return true;
     }
 }
+
+
+class Solution {
+    TreeNode prev = null;
+    public boolean isValidBST(TreeNode root) {
+        return inorder(root);
+    }
+    
+    private boolean inorder(TreeNode root){
+        if(root == null) return true;
+        if(inorder(root.left)==false) return false;
+        if(prev!=null && prev.val >= root.val) return false;
+        prev=root;
+        return inorder(root.right);
+    }
+}
