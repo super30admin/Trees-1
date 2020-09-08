eca3f349fa8fd8f17801d087b4fbb7a0593d7622
Prashant Kariyannavar <kprashantprabhu93@gmail.com>
2020-09-08 22:18:36 +0530

    Valid BST using recursion

--- src/ValidBST.java
+++ src/ValidBST.java
@@ -1,6 +1,8 @@
 import java.util.Stack;
 
 public class ValidBST {
+	TreeNode prev;
+
 	public boolean isValidBST(TreeNode root) {
 		Stack<TreeNode> st = new Stack<TreeNode>();
 		TreeNode prev = null;
@@ -11,7 +13,7 @@ public class ValidBST {
 				root = root.left;
 			}
 			root = st.pop();
-			if (prev!=null && root.val <= prev.val)
+			if (prev != null && root.val <= prev.val)
 				return false;
 			prev = root;
 			root = root.right;
@@ -21,13 +23,38 @@ public class ValidBST {
 
 	}
 
-	public static void main(String[] args) {
+	public boolean isValidBSTRec(TreeNode root) {
+		return inorder(root);
+	}
+
+	private boolean inorder(TreeNode root) {
+		// TODO Auto-generated method stub
+		// base check
 		
-		TreeNode left = new TreeNode(1,null,null);
-		TreeNode right = new TreeNode(3,null,null);
-		TreeNode root = new TreeNode(2,left,right);
+		
+		if (root == null)
+			return true;
+		// logic
+		if (inorder(root.left) == false)
+			return false;// if left sub tree returns false then no need to check on right sub tree..
+//		while (root != null) {
+//			st.push(root);
+//			root = root.left;
+//		}
+		if (prev != null && root.val <= prev.val)
+			return false;
+		prev = root; // also dont intialize prev value as local variable it will dominant and 
+
+		return inorder(root.right);
+	}
+
+	public static void main(String[] args) {
+
+		TreeNode left = new TreeNode(1, null, null);
+		TreeNode right = new TreeNode(3, null, null);
+		TreeNode root = new TreeNode(2, left, right);
 		ValidBST bst = new ValidBST();
-		System.out.println("is my tree BST :"+bst.isValidBST(root));
-	
+		System.out.println("is my tree BST :" + bst.isValidBST(root));
+
 	}
 }
