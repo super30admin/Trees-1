ffba410a52ab4ad4d45d723e7cc163c7afe9f2f5
sidhs5695 <sidhs5695@gmail.com>
2020-09-13 16:09:41 -0400

    Create Construct_BST.java

---
+++ Construct_BST.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length==0)
+            return null;
+         TreeNode root=new TreeNode(preorder[0]);
+        int index=-1;
+        for(int i=0;i<inorder.length;i++)
+            {
+            if(inorder[i]==root.val)
+            {
+            index=i;
+            break;
+        }
+        }
+        System.out.println(root.val+"->");
+        int preLeft[]=Arrays.copyOfRange(preorder,1,index+1);
+        int inLeft[]=Arrays.copyOfRange(inorder,0,index);
+        
+        int preRight[]=Arrays.copyOfRange(preorder,index+1,preorder.length);
+        int inRight[]=Arrays.copyOfRange(inorder,index+1,inorder.length);
+        
+        root.left=buildTree(preLeft,inLeft);
+        root.right=buildTree(preRight,inRight);
+        
+        return root;
+    }
+} 
