fab0fd56082f9d7703fff663fe5ea8edeeb9c0d4
Krunal Trivedi <krunalt79@gmail.com>
2020-10-05 18:29:41 -0700

    Trees-1

---
+++ Solution2.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ // Time : O(N) Space:O(N)
+ // check if root value is more than left node and less then right node....continue this process till end of the node.
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        
+        return helper(root,Long.MIN_VALUE, Long.MAX_VALUE);
+        
+        
+    }
+    
+    
+    public boolean helper(TreeNode root, long min, long max){
+      if (root == null) return true;
+       
+        if (root.val >= max || root.val <= min) return false;
+        
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+}
