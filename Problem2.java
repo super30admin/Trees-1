43f55c1694bb82ca89c12807e793e8b2db88a14b
Hanish Talasila <t.hanishsai@gmail.com>
2020-10-06 14:47:14 -0600

    Create Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,7 @@
+// Time Complexity :O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Edge cases
+
+
+// Your code here along with comments explaining your approach: 
