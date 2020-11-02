12029f40f083675da3f0be1a933483fc983ce70c
Utsav Raychaudhari <utsavraychaudhari@Utsavs-MBP.hsd1.or.comcast.net>
2020-11-02 01:02:20 -0800

    Soltuion to trees-1

---
+++ valid_bst.py
@@ -0,0 +1,25 @@
+# Time Complexity:- O(n)
+# Space Complexity:- O(n)
+# Approach:- Do an inorder traversal of the tree,an inorder traversal of a BST should always be in the sorted order. So while doing this inorder traversal maintain a stack of the elements seen so far and keep on appending to the stack if the element being appended to the stack is less or equal to the topmost element on the stack, it is not a valid BST.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        self.res=[]
+        self.final=True
+        def vbst(root):
+            if root is None:
+                return
+            vbst(root.left)
+            if len(self.res)>=1:
+                if root.val<=self.res[-1]:
+                    self.final=False
+                    return
+            self.res.append(root.val)
+            vbst(root.right)
+        vbst(root)
+        return self.final
