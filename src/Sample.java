02776199dd9b57e6a15ffe23b97257fcd9c1e0a6
pkariya1 <pkariya1@binghamton.edu>
2020-11-03 14:20:20 +0530

    Valid BST completed

---
+++ src/Sample.java
@@ -0,0 +1,11 @@
+// Time Complexity : O(h) i.e. h-->height of the tree
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Three line explanation of solution in plain english
+
+//Used stack to iterate the whole tree
+//pop it when the node hits null
+//check the prev val with root val if its greater/equal then return false
+//else return true
+
+// Your code here along with comments explaining your approach
