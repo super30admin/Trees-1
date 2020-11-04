b3ccc14eecc2f4ee6dd84c9f9444c7fa688f31b6
Aakanksha Patek <aakanksha2696@gmail.com>
2020-11-04 10:15:16 +0530

    Changes in comments in Construct BT problem

--- constructBTfromPreorder&Inorder.py
+++ constructBTfromPreorder&Inorder.py
@@ -2,7 +2,7 @@
 #Space Complexity : O(n) where n in the number of nodes in the tree
 
 #Did this code successfully run on Leetcode : Yes
-#Three line explanation of solution in plain english: Since in preorder root is the first element followed by left and right children. I pop the first element of preorder, making that the root of the tree, then finding that element in inorder (where we know the order is left, root, right), and that ensures that elements to the left of the the root element in inorder is the left subtree and the elements to the right are of the right subtree. This way I way make node.left and node.right of each node and recursively calling the function again and again till elements remain in the inorder array. This will form the entire tree.
+#Three line explanation of solution in plain english: Since in preorder root is the first element followed by left and right children. Maintaining an index, starting from the fromt, I get the "root" from preorder, then finding that element in inorder (where we know the order is left, root, right), and that ensures that elements to the left of the the root element in inorder is the left subtree and the elements to the right are of the right subtree. This way I way make node.left and node.right of each node and recursively calling the function again and again till elements remain in the inorder array. This will form the entire tree.
 
 #Your code here along with comments explaining your approach
 
