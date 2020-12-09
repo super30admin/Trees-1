094d54cf8e72de5a377170a0841bed8af16fe0e9
DivyaPuraswani <divyapuraswani11@gmail.com>
2020-12-08 17:11:33 -0800

    Completed Problem 1 and Problem 2 in Trees2 Repository

---
+++ Problem_2.py
@@ -0,0 +1,12 @@
+# Construct Binary Tree from Preorder and Inorder Traversal
+
+# Time Complexity : 
+# Space Complexity :
+# Did this code successfully run on Leetcode : 
+# Any problem you faced while coding this : 
+# Your code here along with comments explaining your approach :
+# Approach
+"""
+Solved In Trees2 Repository
+
+"""
