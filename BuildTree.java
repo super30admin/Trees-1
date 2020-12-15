dc2fc8a9a6485c0e45926565256be54b81e8b59f
Kavyath Basani <kavyathbasani@gmail.com>
2020-12-15 03:14:35 -0500

    Completed Trees-1 all questions

--- BuildTree.java
+++ BuildTree.java
@@ -1,5 +1,5 @@
-// Time Complexity : O(N * N) 
-// Space Complexity : O(N * N) 
+// Time Complexity : O(N) 
+// Space Complexity : O(N) 
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : NA
 
@@ -22,30 +22,32 @@
  * }
  */
 class Solution {
+    
+    HashMap<Integer, Integer> map;
+    int idx;
+    
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         
-        if(inorder == null || inorder.length == 0) {
+        if(preorder == null || preorder.length == 0) {
             return null;
         }
-        int rootval = preorder[0];
-        TreeNode root = new TreeNode(rootval);
-        int idx = -1;
-        
+        map = new HashMap<>();
         for(int i = 0; i < inorder.length; i++) {
-            if(rootval == inorder[i]) {
-                idx = i;
-            }
+            map.put(inorder[i], i);
         }
-        
-        int[] preLeft = Arrays.copyOfRange(preorder, 1, idx + 1);
-        int[] preRight = Arrays.copyOfRange(preorder, idx + 1, preorder.length);
-        int[] inLeft = Arrays.copyOfRange(inorder, 0, idx);
-        int[] inRight = Arrays.copyOfRange(inorder, idx + 1, inorder.length);
-        
-        root.left = buildTree(preLeft, inLeft);
-        root.right = buildTree(preRight, inRight);
-        
+        return helper(preorder, inorder, 0, inorder.length - 1);
+    }
+    
+    private TreeNode helper(int[] preorder, int[] inorder, int start, int end) {
+        if(start > end) {
+            return null;
+        }
+        int rootVal = preorder[idx];
+        TreeNode root = new TreeNode(rootVal);
+        idx++;
+        int rootIdx = map.get(rootVal);
+        root.left = helper(preorder, inorder, start, rootIdx - 1);
+        root.right = helper(preorder, inorder, rootIdx + 1, end);
         return root;
-        
     }
 }
