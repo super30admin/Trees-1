dcee42d8127e9e4ec8c68d5d2312c3d87113e481
Helisha <helisha.sangani@gmail.com>
2021-01-07 12:59:28 -0700

    Completed-1

--- inpreBuildTree.py
+++ inpreBuildTree.py
@@ -1,3 +1,8 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         '''
--- validBST.py
+++ validBST.py
@@ -1,3 +1,10 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+        
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         '''
