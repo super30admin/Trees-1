7975e78b3c53511fc5bcf497c843f2324b029412
amby2964 <75859704+amby2964@users.noreply.github.com>
2021-01-13 00:50:32 -0700

    Update tree-inorder-preorder.py

--- tree-inorder-preorder.py
+++ tree-inorder-preorder.py
@@ -1,10 +1,16 @@
+'''
+Time Complexity : O(n)
+Space Complexity: O(n)
+
+
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         # inorder L root Right
@@ -14,16 +20,29 @@ class Solution:
         # Need to see solution to understand
         
         # root will be first of preorder
-        if(len(preorder)==0 or len(inorder)==0):
-            return
-        root_val = preorder.pop(0)
-        # first element will always be root in preorder
-        root = TreeNode(root_val)
-        
-        
-        ind = inorder.index(root_val)
-        
-        root.left = self.buildTree(preorder, inorder[:ind])
-        root.right = self.buildTree(preorder, inorder[ind+1:])
-        
-        return root
+        inord = {}
+        for k,v in enumerate(inorder):
+            inord[v] = k
+        preord = deque(preorder)
+        i=0
+        def build(preorder,start,end):
+            
+            if(start>=end):
+                return
+            # root
+            
+            # can perform popleft or use a variable i in global scope
+            #root_val = preorder.popleft()
+            
+            nonlocal i
+            root_val = preorder[i]
+            root = TreeNode(root_val)
+            subtree_ind  = inord[root_val]
+            i+=1
+            # left subtree
+            root.left = build(preorder,start, subtree_ind)
+            # right subtree
+            root.right = build(preorder,subtree_ind+1, end)
+            
+            return root
+        return build(preord, 0, len(preord))
