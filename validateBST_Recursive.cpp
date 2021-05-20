870316b32d560e6df0612f1e804bf5c186a0a73e
Sameer <sameer@umd.edu>
2021-05-20 15:59:57 -0400

    Added bool return type

--- validateBST_Recursive.cpp
+++ validateBST_Recursive.cpp
@@ -42,3 +42,40 @@ public:
         return;
     }
 };
+
+
+//Using Bool return type
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* prev = NULL;
+    bool isValidBST(TreeNode* root) {
+        return helper(root);
+    }
+    
+    bool helper(TreeNode* root){
+    
+        if (root == NULL){
+            return true;
+        }
+
+        if (helper(root->left) == false){
+            return false;
+        } 
+        if (prev != NULL and prev->val >= root->val){
+            return false;
+        }
+        prev = root;
+        return helper(root->right);
+    }
+};
