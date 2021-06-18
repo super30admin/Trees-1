48750179d23a27a2f8c558d27143ca4b5cb242ae
sahil1291sharma <sahil1291sharma@users.noreply.github.com>
2021-06-18 02:35:56 -0400

    update BST validate problem

--- problem1_recursion.py
+++ problem1_recursion.py
@@ -5,29 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    prev = None
+    result = True
     def isValidBST(self, root: TreeNode) -> bool:
-        self.prev = None
-        self.result = True
+
+        
         def inorder(root):
+            #while root != null and stack is not Empty():
             if root == None:
                 return
-            
             inorder(root.left)
-            
+            print("\nPop happens here for left call: ", root.val)
             if self.prev !=None and self.prev >= root.val:
                 self.result = False
-            
+                return
             self.prev = root.val
-            print(root.val)
             inorder(root.right)
+            print("\nPop happens here for right call: ", root.val)
         
         
         inorder(root)
-        return self.result
-    
+        return self.result  
 # Time complexity is O(n) where n is number of nodes
 # Space complexity is O(h) where h is height of the tree
 # recursion is used to compare the node value with previous value
-            
-        
-        
