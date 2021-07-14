52c5ccf79858303ddfcd72f5f27de2700abf1504
jisakp7 <kavya.pathi@jbhunt.com>
2021-07-14 01:11:24 -0500

    BinaryTreeFromInorderPreorderTraversal-Approach2

--- java/BinaryTreeFromInorderPreorderTraversal.java
+++ java/BinaryTreeFromInorderPreorderTraversal.java
@@ -32,6 +32,8 @@
 
 
         15   7
+
+Approach 1
 // Time Complexity : O(N)
 // Space Complexity :O(N)
 // Did this code successfully run on Leetcode : yes
@@ -60,5 +62,61 @@ class Solution {
         root.right=build(preorder,index+1,high);
         return root;
 
+    }
+}
+
+-----------------------------------------------------------------------------
+Approach 2
+// Time Complexity : O(N2)
+// Space Complexity :O(N2)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+/*
+We get the root value from preorders first element and search for that element in inorder and everything left of that index in order would be inleft and right is inRight
+Preorder's preleft would be after the firt element until the length of index found until inorder's rootVal and elements after that would give preRight
+build Tree by reducing the array size in each recursive call
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder==null || inorder==null || preorder.length==0)
+            return null;
+        //find root in preorder
+        int rootVal = preorder[0];
+        TreeNode root = new TreeNode(rootVal);
+
+        int rootIdx =0;
+        //search root in Inorder
+        for(int i=0; i<inorder.length; i++){
+            if(inorder[i] == rootVal)
+                rootIdx = i;
+        }
+
+        int[] preLeft = Arrays.copyOfRange(preorder, 1, rootIdx+1);
+        int[] preRight = Arrays.copyOfRange(preorder, rootIdx+1, preorder.length);
+        int[] inLeft = Arrays.copyOfRange(inorder, 0, rootIdx);
+        int[] inRight = Arrays.copyOfRange(inorder, rootIdx+1, inorder.length);
+        root.left = buildTree(preLeft, inLeft);
+        root.right = buildTree(preRight, inRight);
+        return root;
     }
 }
