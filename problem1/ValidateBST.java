9c135fbcfc8fe5af9aa459acdaeb940e71d25c2a
Srirag <78050630+sriragcs@users.noreply.github.com>
2021-07-13 19:47:18 -0700

    Updated space complexities

--- problem1/ValidateBST.java
+++ problem1/ValidateBST.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(n)
-// Space Complexity : O(n)
+// Space Complexity : O(h), h -> Height of tree
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 package problem1;
