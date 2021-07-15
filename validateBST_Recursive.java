563080d08a6b1f6348ae135f9f5015debaaf6b2e
Prathima Yenumula <54646392+Prathima0407@users.noreply.github.com>
2021-07-15 18:19:23 -0500

    Update validateBST_Recursive.java

--- validateBST_Recursive.java
+++ validateBST_Recursive.java
@@ -1,4 +1,21 @@
 
 public class validateBST_Recursive {
+  TreeNode prev;
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) 
+            return true;
+        return inorder(root);
+    
+    }
+    private boolean inorder(TreeNode root){
+        if(root == null)
+            return true;
+        if(inorder(root.left) == false)
+            return false;
+        if(prev != null && prev.val >= root.val)
+            return false;
+        prev = root;
+        return inorder(root.right);
+    }
 
 }
