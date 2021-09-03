364e0e62a223098583ff6f5929c219a87e97b67d
RaaviSindhu <66458350+RaaviSindhu@users.noreply.github.com>
2021-09-02 21:51:42 -0700

    Add files via upload

---
+++ validate_bst.py
@@ -0,0 +1,22 @@
+# // Time Complexity : O(n)
+# // Space Complexity : 0(n) 
+# // Did this code successfully run on Leetcode : YES
+# // Any problem you faced while coding this : NO
+
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+
+        def validate(node, low=-math.inf, high=math.inf):
+            # Empty trees are valid BSTs.
+            if not node:
+                return True
+            # The current node's value must be between low and high.
+            if node.val <= low or node.val >= high:
+                return False
+
+            # The left and right subtree must also be valid.
+            return (validate(node.right, node.val, high) and
+                   validate(node.left, low, node.val))
+
+        return validate(root)
