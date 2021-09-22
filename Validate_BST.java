b46c4a2a86ed10fa4684c66cf61d7de3146beb33
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2021-09-21 21:01:38 -0400

    Update Validate_BST.java

--- Validate_BST.java
+++ Validate_BST.java
@@ -33,4 +33,51 @@ class Solution {
         return true;
     }
 
+    
+    //Recursion
+    
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // TreeNode prev;
+    public boolean isValidBST(TreeNode root) {
+        // Stack<TreeNode> st = new Stack<>();
+        
+        inorder(root, null);
+        return true;
+        
+      
+    }
+    
+    private boolean inorder(TreeNode root, TreeNode prev){
+        
+        if(root == null) return true;
+        
+         // while(root!= null || !st.isEmpty()){
+        if(inorder(root.left, prev) == false) return false; 
+        // root = st.pop();
+        // System.out.println(root.val);
+            if(prev != null && prev.val >= root.val) return false;
+                prev = root;
+        // root = root.right;  
+                return inorder(root.right, prev);
+        
+    }
+        
+    
+
+}
 }
