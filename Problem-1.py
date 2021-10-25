579c75bb4c6f99b4b65031cb9e9b15015d0b0411
Adhish Shrivastava <adhish70@gmail.com>
2021-10-25 09:39:01 -0400

    Problem1 done

--- Problem-1.py
+++ Problem-1.py
@@ -1,8 +1,8 @@
 # 98. Validate Binary Search Tree
 # https://leetcode.com/problems/validate-binary-search-tree/
 
-# Time complexiety: 
-# Space complexiety: 
+# Time complexiety: O(n)
+# Space complexiety: O(1)
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -11,5 +11,18 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def _helper(self,node,min_val,max_val):
+        if not node:
+            return True
+        
+        if node.val >= max_val or node.val <= min_val:
+            return False
+        
+        left = self._helper(node.left,min_val,node.val)
+        right = self._helper(node.right,node.val,max_val)
+        
+        return left and right
+    
     def isValidBST(self, root) -> bool:
-        pass
+        ptr = root
+        return self._helper(ptr,float("-inf"),float("inf"))
