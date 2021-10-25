ccc90fe72497f0075050083185489f9765f1d416
Adhish Shrivastava <adhish70@gmail.com>
2021-10-25 08:55:46 -0400

    Initial commit

---
+++ Problem-1.py
@@ -0,0 +1,15 @@
+# 98. Validate Binary Search Tree
+# https://leetcode.com/problems/validate-binary-search-tree/
+
+# Time complexiety: 
+# Space complexiety: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root) -> bool:
+        pass
