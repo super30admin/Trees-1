3be839b7f64ee0bf67cc6642540ff36622c3791a
Adhish Shrivastava <adhish70@gmail.com>
2021-10-28 13:46:22 -0400

    Initial commit

--- Problem-42 Construct BST from preorder and inorder.py
+++ Problem-42 Construct BST from preorder and inorder.py
@@ -1,15 +1,24 @@
 # 105. Construct Binary Tree from Preorder and Inorder Traversal
 # https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
-# Time complexiety: 
-# Space complexiety: 
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Recursive Approach
+# Time complexiety: 
+# Space complexiety: 
+class Solution:
+    def buildTree(self, preorder, inorder):
+        pass
+
+
+# Iterative Approach
+# Time complexiety: 
+# Space complexiety: 
 class Solution:
     def buildTree(self, preorder, inorder):
         pass
