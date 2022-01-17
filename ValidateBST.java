4989271fa4f92f728afef6f93e73da6b2fd1bf01
harshaanirudh <harsha.anirudh@gmail.com>
2022-01-16 21:04:32 -0800

    Create ValidateBST.java

---
+++ ValidateBST.java
@@ -0,0 +1,37 @@
+// Time Complexity : O(n)
+// Space Complexity : O(Height)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null)
+            return true;
+        return helper(root, null, null);
+    }
+
+    private boolean helper(TreeNode root, Integer min, Integer max) {
+        if (root == null) {
+            return true;
+        }
+        if ((min != null && root.val <= min) || (max != null && root.val >= max)) {
+            return false;
+        }
+        if (helper(root.left, min, root.val) == false) {
+            return false;
+        }
+        return helper(root.right, root.val, max);
+    }
+}
