bb28b123ed30bda2bbd4cf5ff7f65242922b5c48
mokariv <91024259+mokariv@users.noreply.github.com>
2022-02-23 21:48:11 -0500

    updated complexities

--- ValidateBinarySearchTree98.java
+++ ValidateBinarySearchTree98.java
@@ -1,3 +1,13 @@
+/* Time Complexity : O(n)
+ Space Complexity : O(h)
+   where h : height of tree.
+ Did this code successfully run on Leetcode : yes
+
+Three line explanation of solution in plain english:
+ 
+  Perform inorder traversal. Maintain previous pointer, while traversing a tree check if the current node value is less than the previous node value.
+  If not return false. Else return true.  
+ */
 class Solution {
     boolean isValid = true;
     TreeNode prev = null;
