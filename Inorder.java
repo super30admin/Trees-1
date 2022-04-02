cf10787b88f592927880a3c866388b7b7ffe4720
Het shah <hetshah3008@gmail.com>
2022-04-02 16:53:45 -0400

    reduced the time complexity

--- Inorder.java
+++ Inorder.java
@@ -1,48 +1,54 @@
 
 /*
-Time Complexity :  O(n^2) where finding the index is O(n) and buildig the arrays is O(n).
-Space Complexity: O(1) as we are not using any extra auxillary space.
+Time Complexity :  O(n) where finding the index is O(n).
+Space Complexity: O(n) as I am using Hashmap to store the elements.
 Worked on leetcode.
 */
 import java.util.*;
 
 class Solution {
-    TreeNode root;
-
+    
+    HashMap<Integer,Integer> map;
+    int idx=0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-
-        // base case
-        if (preorder == null || preorder.length == 0)
+        
+        if(preorder.length == 0 || preorder == null) {
+            
             return null;
-
-        // storing root val
-        int rootval = preorder[0];
-        // creating the node
-
-        TreeNode root = new TreeNode(rootval);
-
-        int idx = -1;
-        // finding the index
-        for (int i = 0; i < inorder.length; i++) {
-
-            if (inorder[i] == rootval) {
-
-                idx = i;
-            }
-
         }
-        // building the array
-        int inleft[] = Arrays.copyOfRange(inorder, 0, idx);
-        int inright[] = Arrays.copyOfRange(inorder, idx + 1, inorder.length);
-
-        int preleft[] = Arrays.copyOfRange(preorder, 1, idx + 1);
-        int preright[] = Arrays.copyOfRange(preorder, idx + 1, preorder.length);
-
-        root.left = buildTree(preleft, inleft);
-        root.right = buildTree(preright, inright);
-
-        return root;
-
+        
+        map = new HashMap<>();
+        
+        for(int i  = 0; i<inorder.length;i++) {
+            
+            map.put(inorder[i], i);
+            
+        }
+        
+       return  helper(preorder, 0, inorder.length-1);
+        
+        
     }
-
+    
+    private TreeNode helper(int[]preorder, int start, int end) {
+        
+        if(start>end) {
+            return null;
+        }
+       
+        int rootval = preorder[idx];
+        int rootIdx =  map.get(rootval);
+        idx++;
+        TreeNode root = new TreeNode(rootval);
+        
+        root.left = helper(preorder,start,rootIdx -1);
+        root.right = helper(preorder, rootIdx +1, end);
+        
+        
+        return root;
+        
+        
+    }
+    
+    
 }
