4891f9d8beed1603c2c46e3a4c3c327509fc9307
Rujal <rujal8452@gmail.com>
2022-04-22 01:48:59 -0400

    Problem1

---
+++ Problem1.java
@@ -0,0 +1,82 @@
+public class Problem1 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+//    TC and SC for all the solution
+//    TC : O(n)
+//    Sc : O(h) where h = height of tree
+    class Solution {
+        public boolean isValidBST(TreeNode root) {
+            if (root == null) return true;
+            Stack<TreeNode> s = new Stack<>();
+            TreeNode prev = null;
+            while (root != null || !s.isEmpty()) {
+                while (root != null) {
+                    System.out.println("Adding value " + root.val);
+                    s.push(root);
+                    root = root.left;
+                }
+                root = s.pop();
+                System.out.println("root pop value " + root.val);
+                if (prev != null) {
+                    System.out.println("prev value " + prev.val);
+                }
+                System.out.println("******");
+                if (prev != null && prev.val >= root.val) return false;
+                prev = root;
+                root = root.right;
+            }
+            return true;
+        }
+    }
+
+    //Recursion
+    class Solution2 {
+        Boolean isValid;
+        TreeNode prev = null;
+        public boolean isValidBST(TreeNode root) {
+            isValid = true;
+            inOrder(root);
+            return isValid;
+        }
+        private void inOrder(TreeNode root) {
+            if (root == null) return;
+            inOrder(root.left);
+            if (prev != null && prev.val >= root.val) {
+                isValid = false;
+            }
+            prev = root;
+            inOrder(root.right);
+        }
+    }
+    //recursion with additional boolean check on left node
+    class Solution3 {
+        TreeNode prev = null;
+        public boolean isValidBST(TreeNode root) {
+            return inOrder(root);
+        }
+        private Boolean inOrder(TreeNode root){
+            if (root == null ) return true;
+            if (inOrder(root.left) == false){
+                return false;
+            }
+            if (prev != null && prev.val >= root.val ){
+                return false;
+            }
+            prev = root;
+            return inOrder(root.right);
+        }
+    }
+}
