559a6fe9a400da01919bfd52596d63585106fb1c
eshasah <esha8sah@gmail.com>
2022-04-29 21:47:07 -0700

    soluition updated

--- PreorderInorderTree.java
+++ PreorderInorderTree.java
@@ -1,5 +1,5 @@
-// Time Complexity :O(nxn)
-// Space Complexity :O(h) - heigth of tree, in worst case O(n) - uses stack memory
+// Time Complexity :O(n)
+// Space Complexity :O(n) 
 // Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this :No
 
@@ -7,10 +7,15 @@
 // Your code here along with comments explaining your approach
 
 class PreorderInorderTree {
+    Map<Integer,Integer> map;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         if(preorder == null || inorder == null || preorder.length == 0) return null;
         if(preorder.length == 1) return new TreeNode(preorder[0]);
         
+        map = new HashMap<>();
+        for(int i = 0; i < preorder.length; i++){
+            map.put(inorder[i],i);
+        }
         return helper(preorder, inorder, 0, 0, preorder.length-1);        
     }
     
@@ -22,15 +27,11 @@ class PreorderInorderTree {
         TreeNode node = new TreeNode(preorder[proot]);
         
         //find root in inorder
-        int i = 0;
-        for(i = left; i <= right; i++){
-            if(inorder[i] == preorder[proot]){  
-                break;
-            }
-        }
+        int idx = map.get(preorder[proot]);
+        
         //recurse
-        node.left = helper(preorder, inorder, proot+1, left, i-1);
-        node.right = helper(preorder, inorder, proot+i-left+1, i+1, right);
+        node.left = helper(preorder, inorder, proot+1, left, idx-1);
+        node.right = helper(preorder, inorder, proot+idx-left+1, idx+1, right);
         return node;
     }
 }
