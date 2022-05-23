c65e7d92527fa4ed4a2e2da6a4f24bea8634aa7c
spatil29bing <86124914+spatil29bing@users.noreply.github.com>
2022-05-23 10:23:31 -0400

    Create validateBSTusingboolfunc.py

---
+++ validateBSTusingboolfunc.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    #isvalid = False
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        self.prev = -math.inf
+        #global isvalid
+        #isvalid = True
+        
+        return self.inorder(root)
+    
+    
+    def inorder(self, root: Optional[TreeNode])->bool:
+        if root == None : return True
+        
+        if (self.inorder(root.left) == False): return False
+       
+
+
+
+        if(self.prev!= -math.inf and self.prev >= root.val):
+            return False
+        
+        self.prev = root.val
+        return self.inorder(root.right)
