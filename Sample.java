50d2079faf2bcab20ea233b153a7b3a6def57843
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-14 15:42:38 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,1 +1,33 @@
 
+//****VALIDATE BINARY SEARCH TREE***
+
+class Solution {
+    boolean flag=true;
+    TreeNode prev;
+    public boolean isValidBST(TreeNode root) {
+        inorder(root);
+        return flag;
+        
+        
+    }
+    private void inorder(TreeNode root)
+    {
+        //Base case
+        if(root==null)
+        {
+            return;
+        }
+        
+        //Logic
+        inorder(root.left);
+        if(prev!=null && prev.val>=root.val)
+        {
+            flag=false;
+        }
+        prev=root;
+        if(flag)
+        {
+            inorder(root.right);
+        }
+    }
+}
