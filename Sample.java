32779b71c6ec1ab8c2f073646d46a38b25433d3e
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-15 10:24:46 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -39,21 +39,6 @@ class Solution {
 //Time complexity:o(n);
 //Space complexity:o(h);
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isValidBST(TreeNode root) {
         TreeNode prev=null;
@@ -80,3 +65,37 @@ class Solution {
 }
 
 
+//****VALIDATE BINARY SEARCH TREE- MIN MAX SOLUTION***
+//Time complexity:o(n);
+//Space complexity:o(h);
+//LeetCode runnable:Y
+//Any doubts:N
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+        
+        
+        
+    }
+    
+    private boolean helper(TreeNode root, Integer min, Integer max)
+    {
+        //BASE:
+        if(root==null)
+        {
+            return true;
+        }
+        if(min!=null && root.val<=min) return false;
+        if(max!=null && root.val>=max) return false;
+        
+        
+        //LOGIC:
+        boolean left=helper(root.left, min, root.val);
+        
+        boolean right=helper(root.right, root.val, max);
+        
+        return left && right; 
+        
+    }
+}
