70b62e8484e048d2c22e83b72ca11463fc9e5a7b
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-17 13:58:30 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -99,3 +99,66 @@ class Solution {
         
     }
 }
+
+//****CONSTRUCT BINARY TREE FROM PREORDER AND INORDER- Bruteforce solution****
+//Time complexity: o(n)square;
+//Space complexity: o(n)square;
+//Leetcode runnable: Y;
+
+
+
+//****CONSTRUCT BINARY TREE FROM PREORDER AND INORDER-optimal solution****
+//Time complexity: o(n);
+//Space complexity: o(n);
+//Leetcode runnable: Y;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<Integer, Integer> map;
+    int idx;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+        
+        if(preorder.length==0) return null;
+        map=new HashMap<>();
+        
+        for(int i=0;i<inorder.length;i++)
+        {
+            map.put(inorder[i],i);
+        }
+        return helper(preorder,0,preorder.length-1);
+        
+        
+    }
+    private TreeNode helper(int[] preorder, int start, int end)
+    {
+        
+        //Base
+        if(start>end) return null;
+        
+        //logic
+        int rootval=preorder[idx];
+        TreeNode root=new TreeNode(rootval);
+        idx++;
+        //idx of cuurent root in inorder array
+        int rootidx=map.get(rootval);
+        root.left= helper(preorder, start, rootidx-1);
+        root.right=helper(preorder,rootidx+1, end);
+        return root; 
+        
+    }
+}
