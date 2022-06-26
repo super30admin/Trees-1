8810b0be8c649e1236a8f9917b63cac018bc277d
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-06-25 20:17:57 -0700

    aded ref

--- pre-in.py
+++ pre-in.py
@@ -1,13 +1,14 @@
 # Time Complexity : O(n)
 # Space Complexity : O(n)
 # Did this code successfully run on Leetcode : yes
-# Any problem you faced while coding this :No
+# Any problem you faced while coding this :No (https://www.youtube.com/watch?v=TveLIAOp8Hw)
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder:
