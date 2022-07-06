3953853c288f18aee5c5bd2944d2b7119b65debc
lawzeem <lawzeeml@buffalo.edu>
2022-07-05 19:39:04 -0500

    Final Commit

---
+++ isValidBST.py
@@ -0,0 +1,18 @@
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def valid(node, left, right):
+            if not node:
+                return True
+            elif not (node.val > left and node.val < right):
+                return False
+            return (valid(node.left, left, node.val) and valid(node.right, node.val, right))
+        return valid(root, float("-inf"), float("inf"))
