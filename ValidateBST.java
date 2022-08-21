cc323468bfa4305432c077e6fc4b2d33777eda49
Shivani31996 <shivani.chavan96@gmail.com>
2022-08-21 17:22:07 -0400

    adding approach 2

--- ValidateBST.java
+++ ValidateBST.java
@@ -1,5 +1,6 @@
 //Did this code run on LeetCode: Yes
 
+//Approach 1 - Recursive approach
 public class ValidateBST {
     class TreeNode {
         int val;
@@ -35,4 +36,42 @@ public class ValidateBST {
         prev = root;
         inorder(root.right);
 }
+}
+
+//Iterative approach for inorder traversal
+public class ValidateBST {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+public boolean isValidBST(TreeNode root){
+    Stack<TreeNode> st = new Stack<>();
+
+    TreeNode prev = null;
+
+    while(root != null || !st.isEmpty()){
+        while(root != null){
+            st.push(root);
+            root = root.left;
+        }
+        root = st.pop();
+        if(prev != null && prev.val >= root.val)
+        {
+            return false;
+        }
+        prev = root;
+        root = root.right;
+    }
+    return true;
+}    
 }
