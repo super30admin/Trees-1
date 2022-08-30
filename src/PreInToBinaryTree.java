ac582713834d0dff4b284998b8af76cff3ec97fc
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2022-08-30 15:47:01 -0700

    adding comments

--- src/PreInToBinaryTree.java
+++ src/PreInToBinaryTree.java
@@ -13,22 +13,22 @@ public class PreInToBinaryTree {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         preIndex = 0;
         for(int i=0; i<inorder.length; i++)
-            map.put(inorder[i],i);
+            map.put(inorder[i],i);                                                     // storing all inorder elements' order
         return build(preorder, 0, inorder.length-1);
     }
     private TreeNode build(int[] preorder, int start, int end) {
         if(start > end)
             return null;
-        TreeNode root = new TreeNode(preorder[preIndex++]);
-        if(root == null)
+        TreeNode root = new TreeNode(preorder[preIndex++]);                            // creating new node to be added in a tree(new temp root)
+        if(root == null)                                                            
             return null;
-        if(start == end)
+        if(start == end)                                                               // leaf node
             return root;
 
         int index = map.get(root.val);
 
-        root.left = build(preorder, start, index-1);
-        root.right = build(preorder, index+1, end);
+        root.left = build(preorder, start, index-1);                                   // creating left child subtree
+        root.right = build(preorder, index+1, end);                                    // creating right child subtree
 
         return root;
     }
