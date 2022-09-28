16753bd6eb7dae70bd8c703ea67caf92cd011a6a
Vedant Nibandhe <vnibandhe@gmail.com>
2022-09-28 18:14:45 -0500

    Completed Sprint Trees-1

---
+++ validateBST.py
@@ -0,0 +1,26 @@
+#Time Complexity: O(n)
+#Space Complexity: O(n)
+#Did it run on leetcode: Yes
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        stack = [(root, -math.inf, math.inf)]
+        while stack:
+            root, lower, upper = stack.pop()
+            if not root:
+                continue
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+            stack.append((root.right, val, upper))
+            stack.append((root.left, lower, val))
+        return True
