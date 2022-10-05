84e5285c12c36b0f8da5176edc19bbffcee88a2d
Devanshi Trivedi <32406104+devanshitrivedi@users.noreply.github.com>
2022-10-05 10:05:02 -0700

    Validate bst

---
+++ Problem_2.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
