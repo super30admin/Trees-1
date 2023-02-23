a14026421128ae2841894afd860cf9b64d39aa70
NAVEEN US <88378808+usnaveen@users.noreply.github.com>
2023-02-23 19:07:44 +0530

    Create tree_preorder_inorder.py

---
+++ tree_preorder_inorder.py
@@ -0,0 +1,31 @@
+# Author: Naveen US
+# Title: Construct Binary Tree from Preorder and Inorder Traversal
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+# Did the code run on leetcode: Yes
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        def array_to_tree(left, right):
+            nonlocal preorder_index
+            if left > right: return None
+
+            root_value = preorder[preorder_index]
+            root = TreeNode(root_value)
+
+
+            preorder_index += 1
+
+            root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
+            root.right = array_to_tree(inorder_index_map[root_value] + 1, right)
+
+            return root
+
+        preorder_index = 0
+
+        inorder_index_map = {}
+        for index, value in enumerate(inorder):
+            inorder_index_map[value] = index
+
+        return array_to_tree(0, len(preorder) - 1)
