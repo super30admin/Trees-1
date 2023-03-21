3822daff321ac4b53c596e23df01b724c3ce2aee
Inderpreet Singh <inderpreetsingh@Inderpreets-MacBook-Pro.local>
2023-03-20 19:03:58 -0700

    Trees-1

---
+++ Problem2.java
@@ -0,0 +1,27 @@
+class Solution2 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<inorder.length;i++){
+            map.put(inorder[i],i);
+        }
+        return solve(preorder,0,preorder.length-1,inorder,0,inorder.length-1,map);
+    }
+
+    public TreeNode solve(int[]preorder,int preStart,int preEnd,int[]inorder,int inStart,int inEnd,
+                          HashMap<Integer,Integer>map){
+
+        if(preStart>preEnd || inStart>inEnd) return null;
+
+        TreeNode root=new TreeNode(preorder[preStart]);
+
+        int inRoot=map.get(root.val);
+        int numsLeft=inRoot-inStart;
+
+        root.left=solve(preorder,preStart+1,preStart+numsLeft,inorder,inStart,inRoot-1,map);
+        root.right=solve(preorder,preStart+numsLeft+1,preEnd,inorder,inRoot+1,inEnd,map);
+
+        return root;
+
+    }
+}
