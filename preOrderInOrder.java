0229b7a6a616b59ea9305501b82c486c6bd2b57a
Bharath Barakam <bharath.barakam@ultra.me>
2023-05-08 21:39:43 -0700

    Finished Trees-1

---
+++ preOrderInOrder.java
@@ -0,0 +1,46 @@
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+ }
+
+
+class Solution {
+    int idx;
+     private HashMap<Integer,Integer> map;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(inorder == null || inorder.length == 0 || preorder == null || preorder.length == 0) {
+            return null;
+        }
+
+        map = new HashMap<>();
+
+        for(int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return helper(preorder, 0, preorder.length - 1);
+    }
+    private TreeNode helper(int[] preorder, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int rootVal= preorder[idx];
+        idx++;
+        TreeNode root = new TreeNode(rootVal);
+        int rootIdx = map.get(rootVal);
+        root.left = helper(preorder, start, rootIdx - 1);
+        root.right = helper(preorder, rootIdx+1, end);
+
+        return root;
+    }
+
+}
