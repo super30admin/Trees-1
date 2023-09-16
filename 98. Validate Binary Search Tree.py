685bd9681ffb48c33a079ffcee3df59bb57c9572
Krishna Mandanapu <krishnamandanapu@Krishnas-MacBook-Pro.local>
2023-09-15 19:08:08 -0700

    Completed Trees 1

---
+++ 98. Validate Binary Search Tree.py
@@ -0,0 +1,15 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def valid(node, left, right):
+            if not node:
+                return True
+
+            if not (node.val < right and node.val > left):
+                return False
+
+            return valid(node.left, left, node.val) and valid(node.right, node.val, right)
+
+        return valid(root, float("-inf"), float("inf"))
