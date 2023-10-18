c4fe940ea966883fe0637b1ae4b28aa1e96baf21
Apeksha Kotian <apekshakotian@Apekshas-MBP.attlocal.net>
2023-10-18 10:49:14 -0500

    Completed Trees-1 Valid BST 2.0

--- Sample.java
+++ Sample.java
@@ -1,3 +1,11 @@
+// Time Complexity : O(n)
+// Space Complexity : O(h)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : Gone through the lecture again to work on the solutions and mistakes.
+
+// https://leetcode.com/problems/validate-binary-search-tree
+
+//Valid BST 2.1
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -29,6 +37,53 @@ class Solution {
         }
         prev = root;
         inorder(root.right);
+    }
+    
+}
+// Time Complexity : O(n)
+// Space Complexity : O(h)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : I didnt face any problem while coding this
+
+// https://leetcode.com/problems/validate-binary-search-tree
+
+//Valid BST 2.0
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+        
+    }
+    private boolean helper(TreeNode root, Integer min, Integer max){
+        if(root == null) return true;
+        if(min != null && root.val<=min){
+            return false;
+        }
+        if(max != null && root.val>=max){
+            return false;
+        }
+        
+        boolean left = helper(root.left, min, root.val);
+        if(left == false) return false;
+        boolean right = helper(root.right, root.val, max);
+        return right;
+        
     }
     
 }
