90f6c5c1ae2df7c6b15816d298a14e640f451eec
veeceey <varun_6april@hotmail.com>
2023-11-12 11:12:01 -0800

    adding problems

--- CreateBinaryTreeInorder.py
+++ CreateBinaryTreeInorder.py
@@ -6,14 +6,21 @@ class Node:
 
 
 class Traverse:
+    def isValidBst(self, root):
+        self.prev=None
+        self.result=True
+        self.inorder(root)
+        return self.result
     def inorder(self, root):
         if not root:
             return
         self.inorder(root.left)
         curr=root
+        if self.prev!=None and curr.val<=self.prev.val:
+            self.result = False
+        self.prev = curr
         print(curr.val)
         self.inorder(root.right)
-
 
 node = Node(5)
 node.left = Node(4)
@@ -21,7 +28,7 @@ node.right = Node(6)
 node.left.left= Node(3)
 node.right.right = Node(7)
 solve = Traverse()
-print(solve.inorder(node))
+print(solve.isValidBst(node))
 
 
 
