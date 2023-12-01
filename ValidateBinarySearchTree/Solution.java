9293a55d2389f1d5caa0f7228fd4bfc42c12d31c
26pratik <hariyapratik@gmail.com>
2023-11-30 20:19:10 -0500

    Trees-1

--- ValidateBinarySearchTree/Solution.java
+++ ValidateBinarySearchTree/Solution.java
@@ -13,24 +13,30 @@
  *     }
  * }
  */
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+/**
+ * Recursively traverse through the entire tree in inorder manner. Make a note of the previous node. Check if the value of current
+ * node is less than previous node at each stage. If not, return false.
+ */
 class Solution {
 
     TreeNode prev;
     boolean flag;
 
     public boolean isValidBST(TreeNode root) {
-
         this.flag = true;
-
         validate(root);
-
         return flag;
     }
 
     private void validate(TreeNode root) {
 
-        if(root == null) return;
-        if(!flag) return;
+        if(root == null || !flag) return;
 
         validate(root.left);
 
@@ -44,3 +50,55 @@ class Solution {
         validate(root.right);
     }
 }
+
+/**
+ * converting GLOBAL VARIABLE to local parameter in recursion stack using PASS BY REFERENCE
+ */
+/*
+class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+
+        return validate(root, new TreeNode[1]);
+    }
+
+    private boolean validate(TreeNode root, TreeNode[] prev) {
+
+        if(root == null ) return true;
+
+        if(!validate(root.left, prev)) return false;
+
+        if(prev[0] != null && prev[0].val >= root.val)
+            return false;
+        prev[0] = root;
+
+        return validate(root.right, prev);
+    }
+}*/
+
+
+/**
+ * BOOLEAN BASED RECURSION
+ */
+/*
+class Solution {
+
+    TreeNode prev;
+
+    public boolean isValidBST(TreeNode root) {
+        return validate(root);
+    }
+
+    private boolean validate(TreeNode root) {
+
+        if(root == null ) return true;
+
+        if(!validate(root.left)) return false;
+
+        if(prev != null && prev.val >= root.val)
+            return false;
+        prev = root;
+
+        return validate(root.right);
+    }
+}*/
