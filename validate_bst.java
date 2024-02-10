bfa55b2b8f3d53e1204eaf8c04f04a29c64bab07
Shefali Singh <shefali.singh@gomotive.com>
2024-02-09 22:58:58 -0800

    done trees1

---
+++ validate_bst.java
@@ -0,0 +1,34 @@
+//Time - O(n)
+//Space -  O(h) h=logn worst case O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root,Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean valid(TreeNode root, long min, long max){
+        if(root==null){
+            return true;
+        }
+        else if(root.val>min && root.val<max){
+            return valid(root.left, min, root.val) && valid(root.right, root.val, max);
+        }
+        else{
+            return false;
+        }
+    }
+}
