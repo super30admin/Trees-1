5b8bbc2267c1bb1bff289a226b650ae3614e31eb
SuprajaGandhi <137209418+SuprajaGandhi@users.noreply.github.com>
2024-03-15 13:53:00 -0600

    Completed Trees-1

---
+++ ConstructBST_Preorder.py
@@ -0,0 +1,24 @@
+#TC: O(n)
+#SC: O(h)
+# Here, we find the index of root node from inorder array and slice the inorder and preorder lists in the recursion
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        #Base logic:
+        if not inorder or not preorder:
+            return None
+        root=TreeNode(preorder[0],None,None)
+        mid=inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1],inorder[:mid])
+        root.right=self.buildTree(preorder[mid+1:],inorder[mid+1:])
+
+        return root
+        
+
+        
+        
